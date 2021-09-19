25504cc0022fb1870c1fe150e39348e41bedff9c
Jay Oh <zxc1353@hanmail.net>
2021-09-19 16:39:52 -0400

    Chapter4 - moving Player's head

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 50.0f;
+    public Rigidbody head;
 
     private CharacterController characterController;
 
@@ -19,5 +20,18 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection * moveSpeed);
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (moveDirection == Vector3.zero)
+        {
+
+        }
+        else
+        {
+            head.AddForce(transform.right * 150, ForceMode.Acceleration);
+        }
     }
 }
