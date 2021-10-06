8371e5d0e708fda178482ef7d4234a7062cbdad0
Jay Oh <zxc1353@hanmail.net>
2021-10-06 11:44:54 -0400

    Chapter8 - setting up script to Elevator

---
+++ Assets/Scripts/Arena.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arena : MonoBehaviour
+{
+    public GameObject player;
+    public Transform elevator;
+    private Animator arenaAnimator;
+    private SphereCollider sphereCollider;
+
+    private void Start()
+    {
+        arenaAnimator = GetComponent<Animator>();
+        sphereCollider = GetComponent<SphereCollider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Camera.main.transform.parent.gameObject.GetComponent<CameraMovement>().enabled = false;
+        player.transform.parent = elevator.transform;
+        player.GetComponent<PlayerController>().enabled = false;
+        SoundManager.Instance.PlayOneShot(SoundManager.Instance.elevatorArrived);
+        arenaAnimator.SetBool("OnElevator", true);
+    }
+}
